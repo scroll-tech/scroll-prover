ed30a63cc004b68225fc0204b868371d4cacf00d
Lawliet-Chan <1576710154@qq.com>
2022-05-16 19:02:40 +0800

    some fixs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# common
+# common  
+
+Setup 
+```shell
+cargo build --release --bin setup   
+
+./target/release/setup --params <params-file-path> --seed <seed-file-path>
+```
--- zkevm/src/keygen.rs
+++ zkevm/src/keygen.rs
@@ -9,25 +9,25 @@ use zkevm_circuits::evm_circuit::witness::Block;
 
 /// generate evm verifying key
 pub fn gen_evm_vk(params: &Params<G1Affine>) -> Result<VerifyingKey<G1Affine>, Error> {
-    keygen_vk(&params, &test_circuit())
+    keygen_vk(params, &test_circuit())
 }
 
 /// generate evm proving key
 pub fn gen_evm_pk(params: &Params<G1Affine>) -> Result<ProvingKey<G1Affine>, Error> {
     let evm_vk = gen_evm_vk(params)?;
-    keygen_pk(&params, evm_vk, &test_circuit())
+    keygen_pk(params, evm_vk, &test_circuit())
 }
 
 /// generate state verifying key
 pub fn gen_state_vk(params: &Params<G1Affine>) -> Result<VerifyingKey<G1Affine>, Error> {
     let state_circuit = create_state_circuit();
-    keygen_vk(&params, &state_circuit)
+    keygen_vk(params, &state_circuit)
 }
 
 pub fn gen_state_pk(params: &Params<G1Affine>) -> Result<ProvingKey<G1Affine>, Error> {
     let state_vk = gen_state_vk(params)?;
     let state_circuit = create_state_circuit();
-    keygen_pk(&params, state_vk, &state_circuit)
+    keygen_pk(params, state_vk, &state_circuit)
 }
 
 fn test_circuit() -> impl Circuit<Fr> {
