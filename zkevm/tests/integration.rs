66e8feb78ef7f244a3e4c0327f34c07a4f53bb8d
Lawliet-Chan <1576710154@qq.com>
2022-05-18 17:41:00 +0800

    add logs into tests

--- zkevm/tests/integration.rs
+++ zkevm/tests/integration.rs
@@ -14,10 +14,14 @@ fn test_evm_prove_verify() {
 
     let block_result = mock_block_result();
 
+    log::info!("start generating evm proof");
     let prover = Prover::with_fpath(PARAMS_PATH, SEED_PATH);
     let proof = prover.create_evm_proof(&block_result).unwrap();
+    log::info!("finish generating evm proof");
 
+    log::info!("start verifying evm proof");
     let verifier = Verifier::with_fpath(PARAMS_PATH);
+    log::info!("finish verifying evm proof");
     assert!(verifier.verify_evm_proof(proof, &block_result));
 }
 
@@ -29,9 +33,13 @@ fn test_state_prove_verify() {
 
     let block_result = mock_block_result();
 
+    log::info!("start generating state proof");
     let prover = Prover::with_fpath(PARAMS_PATH, SEED_PATH);
     let proof = prover.create_state_proof(&block_result).unwrap();
+    log::info!("finish generating state proof");
 
+    log::info!("start verifying state proof");
     let verifier = Verifier::with_fpath(PARAMS_PATH);
+    log::info!("finish verifying state proof");
     assert!(verifier.verify_state_proof(proof, &block_result));
 }
