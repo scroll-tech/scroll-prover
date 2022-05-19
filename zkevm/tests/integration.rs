5b608060205623a7716266122c68d594f7b8e687
Lawliet-Chan <1576710154@qq.com>
2022-05-19 10:11:57 +0800

    env_log

--- zkevm/tests/integration.rs
+++ zkevm/tests/integration.rs
@@ -1,59 +1,67 @@
-use std::time::Instant;
-use types::eth::mock_block_result;
-use zkevm::prover::Prover;
-use zkevm::utils::{load_or_create_params, load_or_create_seed};
-use zkevm::verifier::Verifier;
-
-const PARAMS_PATH: &str = "./test_params";
-const SEED_PATH: &str = "./test_seed";
-
 #[cfg(feature = "prove_verify")]
-#[test]
-fn test_evm_prove_verify() {
-    dotenv::dotenv().ok();
-    env_logger::init();
+#[cfg(test)]
+mod tests {
+    use std::sync::Once;
+    use std::time::Instant;
+    use types::eth::mock_block_result;
+    use zkevm::prover::Prover;
+    use zkevm::utils::{load_or_create_params, load_or_create_seed};
+    use zkevm::verifier::Verifier;
 
-    let _ = load_or_create_params(PARAMS_PATH).unwrap();
-    let _ = load_or_create_seed(SEED_PATH).unwrap();
+    const PARAMS_PATH: &str = "./test_params";
+    const SEED_PATH: &str = "./test_seed";
+    static ENV_LOGGER: Once = Once::new();
 
-    let block_result = mock_block_result();
+    fn init() {
+        ENV_LOGGER.call_once(|| env_logger::init());
+    }
 
-    log::info!("start generating evm proof");
-    let now = Instant::now();
-    let prover = Prover::from_fpath(PARAMS_PATH, SEED_PATH);
-    let proof = prover.create_evm_proof(&block_result).unwrap();
-    log::info!("finish generating evm proof, cost {:?}", now.elapsed());
+    #[test]
+    fn test_evm_prove_verify() {
+        dotenv::dotenv().ok();
+        init();
 
-    log::info!("start verifying evm proof");
-    let now = Instant::now();
-    let verifier = Verifier::from_fpath(PARAMS_PATH);
-    log::info!("finish verifying evm proof, cost {:?}", now.elapsed());
-    assert!(verifier.verify_evm_proof(proof, &block_result));
-}
+        let _ = load_or_create_params(PARAMS_PATH).unwrap();
+        let _ = load_or_create_seed(SEED_PATH).unwrap();
 
-#[cfg(feature = "prove_verify")]
-#[test]
-fn test_state_prove_verify() {
-    dotenv::dotenv().ok();
-    env_logger::init();
+        let block_result = mock_block_result();
 
-    let _ = load_or_create_params(PARAMS_PATH).unwrap();
-    let _ = load_or_create_seed(SEED_PATH).unwrap();
+        log::info!("start generating evm proof");
+        let now = Instant::now();
+        let prover = Prover::from_fpath(PARAMS_PATH, SEED_PATH);
+        let proof = prover.create_evm_proof(&block_result).unwrap();
+        log::info!("finish generating evm proof, cost {:?}", now.elapsed());
 
-    let block_result = mock_block_result();
+        log::info!("start verifying evm proof");
+        let now = Instant::now();
+        let verifier = Verifier::from_fpath(PARAMS_PATH);
+        log::info!("finish verifying evm proof, cost {:?}", now.elapsed());
+        assert!(verifier.verify_evm_proof(proof, &block_result));
+    }
 
-    log::info!("start generating state proof");
-    let now = Instant::now();
-    let prover = Prover::from_fpath(PARAMS_PATH, SEED_PATH);
-    let proof = prover.create_state_proof(&block_result).unwrap();
-    log::info!(
-        "finish generating state proof, elapsed: {:?}",
-        now.elapsed()
-    );
+    #[test]
+    fn test_state_prove_verify() {
+        dotenv::dotenv().ok();
+        init();
 
-    log::info!("start verifying state proof");
-    let now = Instant::now();
-    let verifier = Verifier::from_fpath(PARAMS_PATH);
-    log::info!("finish verifying state proof, elapsed: {:?}", now.elapsed());
-    assert!(verifier.verify_state_proof(proof, &block_result));
+        let _ = load_or_create_params(PARAMS_PATH).unwrap();
+        let _ = load_or_create_seed(SEED_PATH).unwrap();
+
+        let block_result = mock_block_result();
+
+        log::info!("start generating state proof");
+        let now = Instant::now();
+        let prover = Prover::from_fpath(PARAMS_PATH, SEED_PATH);
+        let proof = prover.create_state_proof(&block_result).unwrap();
+        log::info!(
+            "finish generating state proof, elapsed: {:?}",
+            now.elapsed()
+        );
+
+        log::info!("start verifying state proof");
+        let now = Instant::now();
+        let verifier = Verifier::from_fpath(PARAMS_PATH);
+        log::info!("finish verifying state proof, elapsed: {:?}", now.elapsed());
+        assert!(verifier.verify_state_proof(proof, &block_result));
+    }
 }
