e46cfe2a261ae5deddcf8e54709d7676a930f2f7
Lawliet-Chan <1576710154@qq.com>
2022-05-19 15:41:36 +0800

    use trace.json instead of mock_blockresult

--- zkevm/tests/integration.rs
+++ zkevm/tests/integration.rs
@@ -1,12 +1,16 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 use std::sync::Once;
 use std::time::Instant;
-use types::eth::mock_block_result;
+use types::eth::BlockResult;
 use zkevm::prover::Prover;
 use zkevm::utils::{load_or_create_params, load_or_create_seed};
 use zkevm::verifier::Verifier;
 
 const PARAMS_PATH: &str = "./test_params";
 const SEED_PATH: &str = "./test_seed";
+const TRACE_PATH: &str = "./tests/trace.json";
 static ENV_LOGGER: Once = Once::new();
 
 fn init() {
@@ -22,7 +26,7 @@ fn test_evm_prove_verify() {
     let _ = load_or_create_params(PARAMS_PATH).unwrap();
     let _ = load_or_create_seed(SEED_PATH).unwrap();
 
-    let block_result = mock_block_result();
+    let block_result = get_block_result_from_file(TRACE_PATH);
 
     log::info!("start generating evm proof");
     let now = Instant::now();
@@ -46,7 +50,7 @@ fn test_state_prove_verify() {
     let _ = load_or_create_params(PARAMS_PATH).unwrap();
     let _ = load_or_create_seed(SEED_PATH).unwrap();
 
-    let block_result = mock_block_result();
+    let block_result = get_block_result_from_file(TRACE_PATH);
 
     log::info!("start generating state proof");
     let now = Instant::now();
@@ -62,4 +66,19 @@ fn test_state_prove_verify() {
     let verifier = Verifier::from_fpath(PARAMS_PATH);
     log::info!("finish verifying state proof, elapsed: {:?}", now.elapsed());
     assert!(verifier.verify_state_proof(proof, &block_result));
+}
+
+fn get_block_result_from_file<P: AsRef<Path>>(path: P) -> BlockResult {
+    let mut buffer = Vec::new();
+    let mut f = File::open(path).unwrap();
+    f.read_to_end(&mut buffer).unwrap();
+
+    #[derive(Deserialize, Serialize, Default)]
+    struct RpcJson {
+        result: crate::protocol::eth::BlockResult,
+    }
+
+    let j = serde_json::from_slice::<RpcJson>(&buffer).unwrap();
+
+    j.result
 }
