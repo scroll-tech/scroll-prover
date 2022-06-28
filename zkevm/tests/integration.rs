2a8c79c063bdaac6a6a0974ea9a098d9d37cd336
HAOYUatHZ <haoyu@protonmail.com>
2022-06-28 16:03:30 +0800

    clean up

--- zkevm/tests/integration.rs
+++ zkevm/tests/integration.rs
@@ -15,8 +15,8 @@ fn parse_trace_path_from_env(mode: &str) -> &'static str {
         "multiple" => "./tests/trace-multiple-erc20.json",
         "native" => "./tests/trace-native-transfer.json",
         "single" => "./tests/trace-single-erc20.json",
-        "dao" => "./tests/dao.json",
-        "nft" => "./tests/nft.json",
+        "dao" => "./tests/trace-dao.json",
+        "nft" => "./tests/trace-nft.json",
         "sushi" => "./tests/trace-masterchef.json",
         _ => "./tests/trace-multiple-erc20.json",
     };
