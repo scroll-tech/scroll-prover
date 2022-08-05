32fbb245f203941c9a650fa5136e8b83619df99c
Lawliet-Chan <1576710154@qq.com>
2022-08-06 00:07:29 +0800

    fix

--- curl_prove.sh
+++ curl_prove.sh
@@ -7,8 +7,8 @@ mkdir -p all_traces
 for i in "${!hashes[@]}"; do
 	echo "-------- Downloading $i ${hashes[$i]}"
 	curl --location --request POST 'https://prealpha.scroll.io/l2' \
-	--header 'Content-Type: application/json' --data-raw '{"jsonrpc": "2.0","method": "eth_getBlockResultByHash","params":["${hashes[$i]}"],"id": 1}' > ${i}.trace
+	--header 'Content-Type: application/json' --data-raw '{"jsonrpc": "2.0","method": "eth_getBlockResultByHash","params":["${hashes[$i]}"],"id": 1}' > ./all_traces/${i}.trace
 	echo "-------- Proving $i ${hashes[$i]}"
-	./target/release/prove --agg  ${i}.proof --params zkevm/test_params --seed zkevm/test_seed
+	./target/release/prove --agg ./all_traces/${i}.proof --params zkevm/test_params --seed zkevm/test_seed
 done
 
