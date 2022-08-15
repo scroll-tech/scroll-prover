65ba32c0d5f8069d5c9be10c74c82c1860a4d4d8
Lawliet-Chan <1576710154@qq.com>
2022-08-15 10:47:37 +0800

    add download script and supply make bridge-test

--- Makefile
+++ Makefile
@@ -36,7 +36,7 @@ test-hash-trace: ## test state circuit with real trace
 
 bridge-test:
 	cargo build --release
-	./target/release/prove --trace=zkevm/tests/traces/bridge --agg=true
+	./target/release/prove --params=./test_params --seed=./test_seed --trace=zkevm/tests/traces/bridge --agg=true
 
 again:
 	MODE=dao cargo test --features prove_verify --release test_evm_prove_verify > $(CURRENTDATE).dao.evm.txt 2>&1; \
---
+++ download_params.sh
@@ -0,0 +1,4 @@
+mkdir -p test_params
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/release-0804-degree25/test_seed
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/release-0804-degree25/test_params/params18 -O ./test_params/params18
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/release-0804-degree25/test_params/params25 -O ./test_params/params26
