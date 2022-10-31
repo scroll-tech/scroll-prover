0e78e5b18f24defe2392f9a1e1b2eb6957f2bf52
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2022-10-31 10:07:36 +0800

    Update download_params.sh

--- contrib/download_params.sh
+++ contrib/download_params.sh
@@ -7,7 +7,7 @@ PROJ_DIR=$DIR"/.."
 pushd $PROJ_DIR
 mkdir -p test_params
 RELEASE_VERSION=release-0920
-wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/test_seed
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/test_seed -O test_seed
 wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/test_params/params18 -O ./test_params/params18
 wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/test_params/params25 -O ./test_params/params25
 popd
