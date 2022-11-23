61ba7d7c8aa197f37dc1b7b56654815921f78d64
Lawliet-Chan <1576710154@qq.com>
2022-11-23 13:18:52 +0800

    add script wget agg_vk

--- contrib/download_params.sh
+++ contrib/download_params.sh
@@ -10,4 +10,5 @@ RELEASE_VERSION=release-0920
 wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/test_seed -O test_seed
 wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/test_params/params18 -O ./test_params/params18
 wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/test_params/params25 -O ./test_params/params25
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/$RELEASE_VERSION/verify_circuit.vkey -O agg_vk
 popd
