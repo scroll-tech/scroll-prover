ded4c47571c78d120b382debc01371a418338d0c
Ho <noelwei@gmail.com>
2023-02-14 13:57:07 +0800

    Update mpt and poseidon circuit (#103)
    * upgrade deps
    
    update deps for clean circuits
    
    for dynamic codehash feat
    
    some trivial fixings
    
    * reset dep to 'scroll-stable'
    
    * update poseidon circuit with phase issue fixing
    
    * update dep
    
    * trivial fix and fmt
    
    * fix missed dep

--- zkevm/src/circuit.rs
+++ zkevm/src/circuit.rs
@@ -3,13 +3,13 @@ use anyhow::bail;
 use halo2_proofs::halo2curves::bn256::Fr;
 use halo2_proofs::plonk::Circuit as Halo2Circuit;
 
-use mpt_circuits::{hash::Hashable, operation::AccountOp, EthTrie, EthTrieCircuit, HashCircuit};
-
 use once_cell::sync::Lazy;
 
 use types::eth::BlockTrace;
 
 use zkevm_circuits::evm_circuit::EvmCircuit as EvmCircuitImpl;
+use zkevm_circuits::mpt_circuit::MptCircuit as ZktrieCircuitImpl;
+use zkevm_circuits::poseidon_circuit::PoseidonCircuit as PoseidonCircuitImpl;
 use zkevm_circuits::state_circuit::StateCircuit as StateCircuitImpl;
 use zkevm_circuits::super_circuit::SuperCircuit as SuperCircuitImpl;
 use zkevm_circuits::util::SubCircuit;
@@ -196,10 +196,7 @@ impl TargetCircuit for StateCircuit {
     }
 }
 
-fn mpt_rows() -> usize {
-    ((1 << *DEGREE) - 10) / <Fr as Hashable>::hash_block_size()
-}
-
+/*
 fn trie_data_from_blocks<'d>(
     block_traces: impl IntoIterator<Item = &'d BlockTrace>,
 ) -> EthTrie<Fr> {
@@ -224,112 +221,56 @@ fn trie_data_from_blocks<'d>(
 
     trie_data
 }
+*/
 
 pub struct ZktrieCircuit {}
 
 impl TargetCircuit for ZktrieCircuit {
-    type Inner = EthTrieCircuit<Fr, true>;
+    type Inner = ZktrieCircuitImpl<Fr>;
 
     fn name() -> String {
         "zktrie".to_string()
     }
-    fn empty() -> Self::Inner {
-        let dummy_trie: EthTrie<Fr> = Default::default();
-        let (circuit, _) = dummy_trie.circuits(mpt_rows());
-        circuit
-    }
-
-    fn from_block_traces(block_traces: &[BlockTrace]) -> anyhow::Result<(Self::Inner, Vec<Vec<Fr>>)>
-    where
-        Self: Sized,
-    {
-        let trie_data = trie_data_from_blocks(block_traces);
-        let (rows, _) = trie_data.use_rows();
-        if rows >= mpt_rows() {
-            bail!("mpt row num overflow: {}", rows);
-        }
-        let (mpt_circuit, _) = trie_data.circuits(mpt_rows());
-        let instance = vec![];
-        Ok((mpt_circuit, instance))
-    }
-
-    fn from_block_trace(block_trace: &BlockTrace) -> anyhow::Result<(Self::Inner, Vec<Vec<Fr>>)>
-    where
-        Self: Sized,
-    {
-        let (mpt_circuit, _) = trie_data_from_blocks(Some(block_trace)).circuits(mpt_rows());
-        let instance = vec![];
-        Ok((mpt_circuit, instance))
-    }
-
-    fn estimate_rows(block_traces: &[BlockTrace]) -> usize {
-        let (mpt_rows, _) = trie_data_from_blocks(block_traces).use_rows();
-        mpt_rows
-    }
-
-    fn get_active_rows(block_traces: &[BlockTrace]) -> (Vec<usize>, Vec<usize>) {
-        // we have compare and pick the maxium for lookup and gate rows, here we
-        // just make sure it not less than 64 (so it has contained all constant rows)
-        let ret = Self::estimate_rows(block_traces);
-        ((0..ret.max(64)).collect(), (0..ret.max(64)).collect())
-    }
 
     fn from_witness_block(
-        _witness_block: &witness::Block<Fr>,
+        witness_block: &witness::Block<Fr>,
     ) -> anyhow::Result<(Self::Inner, Vec<Vec<Fr>>)>
     where
         Self: Sized,
     {
-        todo!()
+        let inner = ZktrieCircuitImpl::new_from_block(witness_block);
+        let instance = vec![];
+        Ok((inner, instance))
+    }
+
+    fn estimate_rows(block_traces: &[BlockTrace]) -> usize {
+        let witness_block = block_traces_to_witness_block(block_traces).unwrap();
+        ZktrieCircuitImpl::min_num_rows_block(&witness_block).0
     }
 }
 
 pub struct PoseidonCircuit {}
 
 impl TargetCircuit for PoseidonCircuit {
-    type Inner = HashCircuit<Fr>;
+    type Inner = PoseidonCircuitImpl<Fr>;
 
     fn name() -> String {
         "poseidon".to_string()
     }
-    fn empty() -> Self::Inner {
-        let dummy_trie: EthTrie<Fr> = Default::default();
-        let (_, circuit) = dummy_trie.circuits(mpt_rows());
-        circuit
-    }
-
-    fn from_block_traces(block_traces: &[BlockTrace]) -> anyhow::Result<(Self::Inner, Vec<Vec<Fr>>)>
-    where
-        Self: Sized,
-    {
-        let trie_data = trie_data_from_blocks(block_traces);
-        //        let (_, rows) = trie_data.use_rows();
-        //        log::info!("poseidon use rows {}", rows);
-        let (_, circuit) = trie_data.circuits(mpt_rows());
-        let instance = vec![];
-        Ok((circuit, instance))
-    }
-
-    fn from_block_trace(block_trace: &BlockTrace) -> anyhow::Result<(Self::Inner, Vec<Vec<Fr>>)>
-    where
-        Self: Sized,
-    {
-        let (_, circuit) = trie_data_from_blocks(Some(block_trace)).circuits(mpt_rows());
-        let instance = vec![];
-        Ok((circuit, instance))
-    }
-
-    fn estimate_rows(block_traces: &[BlockTrace]) -> usize {
-        let (_, rows) = trie_data_from_blocks(block_traces).use_rows();
-        rows
-    }
 
     fn from_witness_block(
-        _witness_block: &witness::Block<Fr>,
+        witness_block: &witness::Block<Fr>,
     ) -> anyhow::Result<(Self::Inner, Vec<Vec<Fr>>)>
     where
         Self: Sized,
     {
-        todo!()
+        let inner = PoseidonCircuitImpl::new_from_block(witness_block);
+        let instance = vec![];
+        Ok((inner, instance))
+    }
+
+    fn estimate_rows(block_traces: &[BlockTrace]) -> usize {
+        let witness_block = block_traces_to_witness_block(block_traces).unwrap();
+        PoseidonCircuitImpl::min_num_rows_block(&witness_block).0
     }
 }
