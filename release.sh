5f3e30757cca7dd7d4aedc3ea7516424c3101dc0
Lawliet-Chan <1576710154@qq.com>
2023-02-21 13:48:11 +0800

    rename zip

--- release.sh
+++ release.sh
@@ -6,5 +6,5 @@ git_commit_id=`git rev-parse --short HEAD`
 cargo build --release
 find target/release | grep libzktrie.so | xargs -i cp {} ./
 cp target/release/libffi.a ./libzkp.a
-zip -r $git_commit_id.zip libzktrie.so libzkp.a
+zip -r libs.zip libzktrie.so libzkp.a
 rm libzktrie.so libzkp.a
