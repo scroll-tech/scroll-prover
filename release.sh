ee611944d59be049c7ac85b4f67aba000bfc3299
Lawliet-Chan <1576710154@qq.com>
2023-02-21 10:18:14 +0800

    fix

--- release.sh
+++ release.sh
@@ -1,11 +1,10 @@
 set -x
 set -e
-set -o pipefail
 
 git_commit_id=`git rev-parse --short HEAD`
 
 cargo build --release
 find target/release | grep libzktrie.so | xargs -i cp {} ./
 cp target/release/libffi.a ./libzkp.a
-zip -r $git_commit_id.zip libzktrie.a libzkp.a
-rm libzktrie.a libzkp.a
+zip -r $git_commit_id.zip libzktrie.so libzkp.a
+rm libzktrie.so libzkp.a
