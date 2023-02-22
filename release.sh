42d51e68550298871e7c3d3d3541f8ce43bf7b1c
Lawliet-Chan <1576710154@qq.com>
2023-02-22 10:56:53 +0800

    add sha256sum

--- release.sh
+++ release.sh
@@ -6,5 +6,8 @@ set -e
 cargo build --release
 find target/release | grep libzktrie.so | xargs -i cp {} ./
 cp target/release/libffi.a ./libzkp.a
-zip -r libs.zip libzktrie.so libzkp.a
-rm libzktrie.so libzkp.a
+shasum -a 256 libzkp.a > libs.sha256
+shasum -a 256 libzktrie.so >> libs.sha256
+zip -r libs.zip libzktrie.so libzkp.a libs.sha256
+rm libzktrie.so libzkp.a libs.sha256
+shasum -a 256 libs.zip > zip.sha256
