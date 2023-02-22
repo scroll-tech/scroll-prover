f822be0e6fadd6e15454981e88ecabfa7915a3be
Lawliet-Chan <1576710154@qq.com>
2023-02-22 10:58:03 +0800

    format

--- release.sh
+++ release.sh
@@ -6,8 +6,11 @@ set -e
 cargo build --release
 find target/release | grep libzktrie.so | xargs -i cp {} ./
 cp target/release/libffi.a ./libzkp.a
+
 shasum -a 256 libzkp.a > libs.sha256
 shasum -a 256 libzktrie.so >> libs.sha256
+
 zip -r libs.zip libzktrie.so libzkp.a libs.sha256
-rm libzktrie.so libzkp.a libs.sha256
 shasum -a 256 libs.zip > zip.sha256
+
+rm libzktrie.so libzkp.a libs.sha256
