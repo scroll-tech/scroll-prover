d7eb6583108c7d8b0045a82b3419bc3b2ff95cfc
Lawliet-Chan <1576710154@qq.com>
2023-02-28 14:08:07 +0800

    feat: add ffi for scroll; add auto-release for tag (#109)
    * add ffi for scroll
    
    * add release.sh
    
    * fix
    
    * rename zip
    
    * add workflow release.yml
    
    * fix action
    
    * add sha256sum
    
    * format
    
    * fix
    
    * modify
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

---
+++ ffi/src/prove.rs
@@ -0,0 +1,46 @@
+use crate::utils::{c_char_to_str, c_char_to_vec, vec_to_c_char};
+use libc::c_char;
+use std::cell::OnceCell;
+use types::eth::BlockTrace;
+use zkevm::prover::Prover;
+
+static mut PROVER: OnceCell<Prover> = OnceCell::new();
+
+/// # Safety
+#[no_mangle]
+pub unsafe extern "C" fn init_prover(params_path: *const c_char, seed_path: *const c_char) {
+    env_logger::init();
+
+    let params_path = c_char_to_str(params_path);
+    let seed_path = c_char_to_str(seed_path);
+    let p = Prover::from_fpath(params_path, seed_path);
+    PROVER.set(p).unwrap();
+}
+
+/// # Safety
+#[no_mangle]
+pub unsafe extern "C" fn create_agg_proof(trace_char: *const c_char) -> *const c_char {
+    let trace_vec = c_char_to_vec(trace_char);
+    let trace = serde_json::from_slice::<BlockTrace>(&trace_vec).unwrap();
+    let proof = PROVER
+        .get_mut()
+        .unwrap()
+        .create_agg_circuit_proof(&trace)
+        .unwrap();
+    let proof_bytes = serde_json::to_vec(&proof).unwrap();
+    vec_to_c_char(proof_bytes)
+}
+
+/// # Safety
+#[no_mangle]
+pub unsafe extern "C" fn create_agg_proof_multi(trace_char: *const c_char) -> *const c_char {
+    let trace_vec = c_char_to_vec(trace_char);
+    let traces = serde_json::from_slice::<Vec<BlockTrace>>(&trace_vec).unwrap();
+    let proof = PROVER
+        .get_mut()
+        .unwrap()
+        .create_agg_circuit_proof_batch(traces.as_slice())
+        .unwrap();
+    let proof_bytes = serde_json::to_vec(&proof).unwrap();
+    vec_to_c_char(proof_bytes)
+}
