ceff67783e3c0507b9a184265ccf1d8ca76df517
Zhang Zhuo <mycinbrin@gmail.com>
2023-05-24 15:26:57 +0800

    fix default trace

--- zkevm/src/test_util.rs
+++ zkevm/src/test_util.rs
@@ -72,12 +72,12 @@ pub fn parse_trace_path_from_mode(mode: &str) -> &'static str {
         "empty" => "./tests/traces/empty.json",
         "greeter" => "./tests/traces/greeter.json",
         "single" => "./tests/traces/erc20/single.json",
-        "multiple" => "./tests/traces/erc20/multiple.json",
+        "multiple" => "./tests/traces/erc20/erc20_10_transfer.json",
         "native" => "./tests/traces/native_transfer.json",
         "dao" => "./tests/traces/dao/propose.json",
         "nft" => "./tests/traces/nft/mint.json",
         "sushi" => "./tests/traces/sushi/chef_withdraw.json",
-        _ => "./tests/traces/erc20/multiple.json",
+        _ => "./tests/traces/erc20/erc20_10_transfer.json",
     };
     log::info!("using mode {:?}, testing with {:?}", mode, trace_path);
     trace_path
