d59ea1d192e93be5cc1cfa888e9de56265f7830f
Zhuo Zhang <mycinbrin@gmail.com>
2023-05-29 05:42:44 +0000

    release v0.3.1

--- zkevm/release.sh
+++ zkevm/release.sh
@@ -1,31 +1,22 @@
 set -x
 set -e
+set -u
 set -o pipefail
 
 export OPT_MEM=true
 #export MOCK_PROVE=true
-#export KECCAK_ROWS=20
-#export KECCAK_DEGREE=19
 export RUST_MIN_STACK=100000000
-export PARAM_SEED=bb4b94a1bbef58c4b5fcda6c900629b5 
 
-function goerli() {
-		TRACE_DIR="0327-correct-deletion"
-		d="617365.json"
-		RUST_LOG=debug TRACE_PATH=`realpath ~/zip-traces/${TRACE_DIR}/${d}` cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.${d}.log
-		d="617366.json"
-		RUST_LOG=debug TRACE_PATH=`realpath ~/zip-traces/${TRACE_DIR}/${d}` cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.${d}.log
+function run_goerli_tests() {
+	MOCK_PROVE=true RUST_LOG=debug TRACE_PATH=$(realpath tests/extra_traces/hash_precompile_1.json) cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/precompile_fail.log
+
+	#RUST_LOG=debug MODE=multi cargo test --features prove_verify --release test_aggregation_api -- --nocapture 2>&1 | tee logs/multi.log
+	#RUST_LOG=debug MODE=sushi cargo test --features prove_verify --release test_aggregation_api -- --nocapture 2>&1 | tee logs/sushi.log
 }
 
-function run_agg_tests() {
-	#RUST_LOG=info MODE=single cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.log.single
-	#RUST_LOG=debug MODE=greeter cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.log.greeter
-	#RUST_LOG=debug MODE=pack cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.log.pack
-	RUST_LOG=debug MODE=multi cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.log.multi
-	RUST_LOG=debug MODE=sushi cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.log.sushi
-	#RUST_LOG=trace PARAM_SEED=bb4b94a1bbef58c4b5fcda6c900629b5 MODE=native cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/agg.log.native
+function run_devnet_tests() {
+	CHAIN_ID=5343532222 RUST_LOG=debug TRACE_PATH=$(realpath tests/extra_traces/devnet.json) cargo test --features prove_verify --release test_agg -- --nocapture 2>&1 | tee logs/devnet.log
 }
 
-goerli
-#run_agg_tests
-
+run_goerli_tests
+run_devnet_tests
