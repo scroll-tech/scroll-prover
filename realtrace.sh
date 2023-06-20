420112e99a41f26fd32279b6b353dd4829a37d2c
Velaciela <git.rover@outlook.com>
2023-06-20 12:36:33 +0000

    parallel assignment integration test

--- realtrace.sh
+++ realtrace.sh
@@ -11,7 +11,7 @@ export PARAM_SEED=bb4b94a1bbef58c4b5fcda6c900629b5
 
 function realtrace() {
 		d="617365.json"
-		RUST_LOG=info TRACE_PATH=`realpath ./${d}` cargo test --features prove_verify --release test_aggregation_api -- --nocapture 2>&1 | tee agg.${d}.log
+		RUST_LOG=debug ASSIGNMENT_TYPE=parallel TRACE_PATH=`realpath ./${d}` cargo test --features prove_verify --release test_aggregation_api -- --nocapture 2>&1 | tee agg.${d}.log
 }
 
 realtrace
