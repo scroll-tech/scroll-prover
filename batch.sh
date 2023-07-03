8765184d23d67d9c46125b35196bbdb92a9f4775
Zhuo Zhang <mycinbrin@gmail.com>
2023-07-03 09:43:34 +0000

    fail..

--- batch.sh
+++ batch.sh
@@ -1,23 +1,30 @@
 set -x
 set -u
-#set -e 
+#set -e
 set -o pipefail
 
 export RUST_LOG=trace
 
+function simple_tests() {
+	for mode in multiple; do #sushi pack nft dao empty native
+		MODE=$mode make mock 2>&1 | tee /tmp/mock_${mode}.log
+	done
+}
+
 function check_batch() {
 	TRACE_VER="0317-alpha"
 	for d in $(ls ~/zip-traces/${TRACE_VER}/traces); do
-		export TRACE_PATH=$(realpath ~/zip-traces/${TRACE_VER}/traces/${d}/traces-data) 
+		export TRACE_PATH=$(realpath ~/zip-traces/${TRACE_VER}/traces/${d}/traces-data)
 		make mock 2>&1 | tee /tmp/mock_${d}.log
 	done
 }
 
 function check_block() {
 	for t in prover/tests/extra_traces/tx_storage_proof.json prover/tests/extra_traces/hash_precompile_2.json prover/tests/extra_traces/hash_precompile_1.json prover/tests/traces/sushi/sushi_chef-withdraw.json prover/tests/traces/erc20/erc20_10_transfer.json; do
-		TRACE_PATH=`realpath $t` make mock 2>&1 | tee /tmp/mock_`basename $t`.log
+		TRACE_PATH=$(realpath $t) make mock 2>&1 | tee /tmp/mock_$(basename $t).log
 	done
 }
 
-check_block
+simple_tests
+#check_block
 #check_batch
