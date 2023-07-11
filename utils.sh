976c7d7054ebf98db2cf97a816203320f851e23e
Zhang Zhuo <mycinbrin@gmail.com>
2023-07-11 13:04:19 +0800

    switch to shanghai traces when testing (#161)
    * 2 mpt lookup fail
    
    * bridge traces can pass mock proving
    
    * done

---
+++ utils.sh
@@ -0,0 +1,30 @@
+set -x
+set -u
+#set -e
+set -o pipefail
+
+export RUST_LOG=debug
+#export RUST_LOG=trace
+
+function simple_tests() {
+	for mode in pack sushi multiple nft dao native empty 
+	do
+		#MODE=$mode make mock 2>&1 | tee /tmp/mock_${mode}.log
+		(MODE=$mode make mock > /tmp/mock_${mode}.log 2>&1) &
+	done
+	wait
+	echo test done
+	grep 'proved' /tmp/mock_*.log
+}
+
+function replace_zkevm_circuits_branch() {
+	FROM=feat/deploy_at_existed_acc
+	TO=develop
+	sed -i 's#zkevm-circuits.git", branch = "'$FROM'#zkevm-circuits.git", branch = "'$TO'#' */Cargo.toml
+	cargo update -p zkevm-circuits
+	cargo update -p eth-types
+	git diff */Cargo.toml
+}
+
+#replace_zkevm_circuits_branch
+simple_tests
