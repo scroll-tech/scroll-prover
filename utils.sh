954d4650419ab64832c57749ee27cf52c8ce0d35
Zhuo Zhang <mycinbrin@gmail.com>
2023-07-11 04:53:38 +0000

    done

--- utils.sh
+++ utils.sh
@@ -1,22 +1,28 @@
 set -x
 set -u
-set -e
+#set -e
 set -o pipefail
 
-#export RUST_LOG=debug
-export RUST_LOG=trace
+export RUST_LOG=debug
+#export RUST_LOG=trace
 
 function simple_tests() {
-	for mode in pack #sushi multiple #nft dao native empty # pack
-	#for mode in native empty # pack
+	for mode in pack sushi multiple nft dao native empty 
 	do
-		MODE=$mode make mock 2>&1 | tee /tmp/mock_${mode}.log
+		#MODE=$mode make mock 2>&1 | tee /tmp/mock_${mode}.log
+		(MODE=$mode make mock > /tmp/mock_${mode}.log 2>&1) &
 	done
+	wait
+	echo test done
+	grep 'proved' /tmp/mock_*.log
 }
 
 function replace_zkevm_circuits_branch() {
-	BRANCH=feat/deploy_at_existed_acc
-	sed -i 's#zkevm-circuits.git", branch = "develop#zkevm-circuits.git", branch = "'$BRANCH'#' */Cargo.toml
+	FROM=feat/deploy_at_existed_acc
+	TO=develop
+	sed -i 's#zkevm-circuits.git", branch = "'$FROM'#zkevm-circuits.git", branch = "'$TO'#' */Cargo.toml
+	cargo update -p zkevm-circuits
+	cargo update -p eth-types
 	git diff */Cargo.toml
 }
 
