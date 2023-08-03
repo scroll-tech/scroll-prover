f08d64374b78c104ff2997d5ec8ca31860dfc1ee
Steven <asongala@163.com>
2023-08-03 12:53:42 +0800

    fix: delete FFI related in release script (#195)
    Delete FFI related.

--- release.sh
+++ release.sh
@@ -5,12 +5,10 @@ set -e
 
 cargo build --release
 find target/release | grep libzktrie.so | xargs -i cp {} ./
-cp target/release/libffi.a ./libzkp.a
 
-shasum -a 256 libzkp.a > zkp.sha256
 shasum -a 256 libzktrie.so > zktrie.sha256
 
-zip -r libs.zip libzktrie.so libzkp.a zkp.sha256 zktrie.sha256
+zip -r libs.zip libzktrie.so zktrie.sha256
 shasum -a 256 libs.zip > zip.sha256
 
-rm libzktrie.so libzkp.a zkp.sha256 zktrie.sha256
+rm libzktrie.so zktrie.sha256
