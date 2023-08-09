36ef05ee9eae611dc378e97fb8b9c67faa0a6718
Steven Gu <asongala@163.com>
2023-08-09 12:25:06 +0800

    U

--- prover/tests/aggregation_tests.rs
+++ prover/tests/aggregation_tests.rs
@@ -1,15 +1,13 @@
 use aggregator::CompressionCircuit;
 use prover::{
-    aggregator::{Prover, Verifier},
     common,
     config::{LAYER4_CONFIG_PATH, LAYER4_DEGREE},
     proof::from_json_file,
-    test_util::{load_block_traces_for_test, PARAMS_DIR},
-    utils::{chunk_trace_to_witness_block, init_env_and_log, load_params},
-    zkevm, BatchProof, ChunkHash, ChunkProof, EvmProof, Proof,
+    test_util::PARAMS_DIR,
+    utils::{init_env_and_log, load_params},
+    EvmProof,
 };
-use serde_derive::{Deserialize, Serialize};
-use snark_verifier_sdk::Snark;
+use snark_verifier_sdk::rust_verify;
 use std::env;
 
 #[cfg(feature = "prove_verify")]
@@ -27,7 +25,17 @@ fn gen_and_verify_evm_proof(output_dir: &str) {
 
     env::set_var("COMPRESSION_CONFIG", &*LAYER4_CONFIG_PATH);
     let vk = evm_proof.proof.vk::<CompressionCircuit>();
+
     let params = load_params(PARAMS_DIR, *LAYER4_DEGREE, None).unwrap();
+
+    let accept = rust_verify(
+        &params,
+        &vk,
+        &evm_proof.proof.instances(),
+        evm_proof.proof.proof(),
+    );
+    assert!(accept);
+
     common::Verifier::<CompressionCircuit>::new(params, vk).evm_verify(&evm_proof, &output_dir);
     log::info!("Generated deployment bytecode");
 }
