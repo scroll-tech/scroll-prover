a53d38d36ad5720345b28b127b0bdaff0b643f28
Steven Gu <asongala@163.com>
2023-08-16 13:40:48 +0800

    Update

--- prover/tests/integration.rs
+++ prover/tests/integration.rs
@@ -10,7 +10,6 @@ use prover::{
     test_util::{load_block_traces_for_test, parse_trace_path_from_mode, PARAMS_DIR},
     utils::{
         get_block_trace_from_file, init_env_and_log, load_params, short_git_version,
-        GIT_VERSION_SHORT_LEN,
     },
     zkevm::{
         circuit::{block_traces_to_padding_witness_block, SuperCircuit, TargetCircuit},
@@ -24,9 +23,9 @@ fn test_short_git_version() {
     init_env_and_log("integration");
 
     let git_version = short_git_version();
-    log::info!("git_version = {git_version}");
+    log::info!("short_git_version = {git_version}");
 
-    assert_eq!(git_version.len(), GIT_VERSION_SHORT_LEN);
+    assert_eq!(git_version.len(), 7);
 }
 
 #[ignore]
