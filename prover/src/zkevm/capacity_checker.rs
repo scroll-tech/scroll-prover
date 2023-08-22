cdee9ea69f6d4524ee99f29a4cc945b2b2e67079
Zhuo Zhang <mycinbrin@gmail.com>
2023-08-22 03:21:31 +0000

    minor

--- prover/src/zkevm/capacity_checker.rs
+++ prover/src/zkevm/capacity_checker.rs
@@ -148,6 +148,7 @@ impl CircuitCapacityChecker {
         self.state = None;
         self.acc_row_usage = RowUsage::new();
         self.row_usages = Vec::new();
+        self.codelen = HashMap::new();
     }
     pub fn estimate_circuit_capacity(
         &mut self,
