07cd58553c104d81a7696daf3469a5dc75f61d50
zhenfei <zhenfei.zhang@hotmail.com>
2023-09-18 18:45:11 -0400

    write output srs

--- bin/src/re_rand_srs.rs
+++ bin/src/re_rand_srs.rs
@@ -1,5 +1,8 @@
+use std::{fs::File, io::BufWriter};
+
 use anyhow::Result;
 use clap::Parser;
+use halo2_proofs::poly::commitment::Params;
 use prover::utils::{load_params, re_randomize_srs};
 use tiny_keccak::{Hasher, Keccak};
 
@@ -26,15 +29,18 @@ fn main() -> Result<()> {
 
     log::info!("Re-randomize SRS in file: {}", args.input_param);
     log::info!("Degree of SRS: {}", args.degree);
-    log::info!("Using seed: {}", args.seed);
-    log::info!("Output new SRS to: {}", args.output_param);
 
-    let mut old_param = load_params(&args.input_param, args.degree as u32, None)?;
+    let mut param = load_params(&args.input_param, args.degree as u32, None)?;
+
+    log::info!("Using seed: {}", args.seed);
     let mut hasher = Keccak::v256();
     hasher.update(args.seed.as_bytes());
     let mut seed = [0u8; 32];
     hasher.finalize(&mut seed);
-    re_randomize_srs(&mut old_param, &seed);
+    re_randomize_srs(&mut param, &seed);
+
+    log::info!("Output new SRS to: {}", args.output_param);
+    param.write(&mut BufWriter::new(File::create(args.output_param)?))?;
 
     log::info!("SRS re-randomization finished");
     Ok(())
