92a18fff9b8380b4f803dbe629adc38c4ce4222c
Steven Gu <asongala@163.com>
2023-10-25 11:15:09 +0800

    Test mem-usage with chunk-tests.

--- integration/tests/chunk_tests.rs
+++ integration/tests/chunk_tests.rs
@@ -1,11 +1,6 @@
-use integration::test_util::{
-    gen_and_verify_chunk_proofs, load_block_traces_for_test, ASSETS_DIR, PARAMS_DIR,
-};
-use prover::{
-    utils::{chunk_trace_to_witness_block, init_env_and_log},
-    zkevm::Prover,
-};
-use std::env;
+use integration::test_util::{load_block_traces_for_test, ASSETS_DIR, PARAMS_DIR};
+use prover::{utils::init_env_and_log, zkevm::Prover};
+use std::process::{Command, Stdio};
 
 #[cfg(feature = "prove_verify")]
 #[test]
@@ -13,21 +8,27 @@ fn test_chunk_prove_verify() {
     let output_dir = init_env_and_log("chunk_tests");
     log::info!("Initialized ENV and created output-dir {output_dir}");
 
+    let mut prover = Prover::from_dirs(PARAMS_DIR, ASSETS_DIR);
+    log::info!("Constructed chunk prover");
+
     let chunk_trace = load_block_traces_for_test().1;
     log::info!("Loaded chunk trace");
 
-    let witness_block = chunk_trace_to_witness_block(chunk_trace).unwrap();
-    log::info!("Got witness block");
+    for i in 0..50 {
+        log::info!("Proof-{i} BEGIN mem: {}", mem_usage());
+        prover
+            .gen_chunk_proof(chunk_trace.clone(), None, None, None)
+            .unwrap();
+        log::info!("Proof-{i} END mem: {}", mem_usage());
+    }
+}
 
-    env::set_var("CHUNK_VK_FILENAME", "vk_chunk_0.vkey");
-    let mut zkevm_prover = Prover::from_dirs(PARAMS_DIR, ASSETS_DIR);
-    log::info!("Constructed zkevm prover");
-
-    // Load or generate compression wide snark (layer-1).
-    let layer1_snark = zkevm_prover
-        .inner
-        .load_or_gen_last_chunk_snark("layer1", &witness_block, None, Some(&output_dir))
+fn mem_usage() -> String {
+    let cmd = Command::new("echo")
+        .stdout(Stdio::piped())
+        .arg("$(date '+%Y-%m-%d %H:%M:%S') $(free -g | grep Mem: | sed 's/Mem://g')")
+        .spawn()
         .unwrap();
-
-    gen_and_verify_chunk_proofs(&mut zkevm_prover, layer1_snark, &output_dir);
+    let output = cmd.wait_with_output().unwrap().stdout;
+    String::from_utf8_lossy(&output).to_string()
 }
