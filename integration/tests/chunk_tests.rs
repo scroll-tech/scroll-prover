93e72afebba97c60064f11bfdcd784dd3d6f19d5
kunxian-xia <xiakunxian130@gmail.com>
2024-01-25 19:29:24 +0800

    run loop in chunk test

--- integration/tests/chunk_tests.rs
+++ integration/tests/chunk_tests.rs
@@ -1,11 +1,8 @@
-use integration::test_util::{
-    gen_and_verify_chunk_proofs, load_block_traces_for_test, ASSETS_DIR, PARAMS_DIR,
-};
-use prover::{
-    utils::{chunk_trace_to_witness_block, init_env_and_log},
-    zkevm::Prover,
-};
-use std::env;
+use integration::test_util::{load_block_traces_for_test, ASSETS_DIR, PARAMS_DIR};
+use prover::{utils::init_env_and_log, zkevm::Prover};
+use std::process::{Command, Stdio};
+
+static mut PROVER: Option<Prover> = None;
 
 #[cfg(feature = "prove_verify")]
 #[test]
@@ -13,21 +10,30 @@ fn test_chunk_prove_verify() {
     let output_dir = init_env_and_log("chunk_tests");
     log::info!("Initialized ENV and created output-dir {output_dir}");
 
+    let prover = Prover::from_dirs(PARAMS_DIR, ASSETS_DIR);
+    unsafe {
+        PROVER = Some(prover);
+    }
+    log::info!("Constructed chunk prover");
+
     let chunk_trace = load_block_traces_for_test().1;
     log::info!("Loaded chunk trace");
 
-    let witness_block = chunk_trace_to_witness_block(chunk_trace).unwrap();
-    log::info!("Got witness block");
+    for i in 0..50 {
+        log::info!("Proof-{i} BEGIN mem: {}", mem_usage());
+        unsafe {
+            PROVER
+                .as_mut()
+                .unwrap()
+                .gen_chunk_proof(chunk_trace.clone(), None, None, None);
+            // .unwrap();
+        }
+        log::info!("Proof-{i} END mem: {}", mem_usage());
+    }
+}
 
-    env::set_var("CHUNK_VK_FILENAME", "vk_chunk_0.vkey");
-    let mut zkevm_prover = Prover::from_dirs(PARAMS_DIR, ASSETS_DIR);
-    log::info!("Constructed zkevm prover");
-
-    // Load or generate compression wide snark (layer-1).
-    let layer1_snark = zkevm_prover
-        .inner
-        .load_or_gen_last_chunk_snark("layer1", &witness_block, None, Some(&output_dir))
-        .unwrap();
-
-    gen_and_verify_chunk_proofs(&mut zkevm_prover, layer1_snark, &output_dir);
+fn mem_usage() -> String {
+    let cmd = "echo \"$(date '+%Y-%m-%d %H:%M:%S') $(free -g | grep Mem: | sed 's/Mem://g')\"";
+    let output = Command::new("bash").arg("-c").arg(cmd).output().unwrap();
+    String::from_utf8_lossy(&output.stdout).to_string()
 }
