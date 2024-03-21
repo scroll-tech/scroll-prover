6cc64b2fa09dc5dab0cabb5dda68a54e622a50d9
Zhuo Zhang <mycinbrin@gmail.com>
2024-03-21 07:35:02 +0000

    improve ccc

--- integration/tests/integration.rs
+++ integration/tests/integration.rs
@@ -3,6 +3,7 @@ use halo2_proofs::{
     poly::commitment::Params,
 };
 use integration::test_util::{
+    ccc_as_signer,
     load_block_traces_for_test, parse_trace_path_from_mode, prepare_circuit_capacity_checker,
     run_circuit_capacity_checker, PARAMS_DIR,
 };
@@ -67,28 +68,38 @@ fn test_cs_same_for_vk_consistent() {
 #[test]
 fn test_capacity_checker() {
     init_env_and_log("integration");
-
-    let trace_path = parse_trace_path_from_mode("multiswap");
-    // let trace_path = "./tests/extra_traces/new.json";
-
     prepare_circuit_capacity_checker();
 
-    let block_traces = vec![get_block_trace_from_file(trace_path)];
-    let witness_block = block_traces_to_witness_block(block_traces.clone()).unwrap();
+    let block_traces = load_block_traces_for_test().1;
 
-    let avg_each_tx_time = run_circuit_capacity_checker(0, 0, &block_traces, &witness_block);
-    assert!(avg_each_tx_time < Duration::from_millis(100));
+    let full = false;
+    let batch_id = 0;
+    let chunk_id = 0;
+    let avg_each_tx_time = if full {
+        let witness_block = block_traces_to_witness_block(block_traces.clone()).unwrap();
+        let avg_each_tx_time = run_circuit_capacity_checker(batch_id, chunk_id, &block_traces, &witness_block);
+        avg_each_tx_time
+    } else {
+        ccc_as_signer(chunk_id, &block_traces).1
+    };
+    log::info!("avg_each_tx_time {avg_each_tx_time:?}");
 }
 
 #[test]
 fn estimate_circuit_rows() {
     init_env_and_log("integration");
-
+    prepare_circuit_capacity_checker();
+    
     let (_, block_trace) = load_block_traces_for_test();
-
-    log::info!("estimating used rows for batch");
-    let rows = SuperCircuit::estimate_rows(block_trace);
-    log::info!("super circuit: {:?}", rows);
+    let witness_block = block_traces_to_witness_block(block_trace).unwrap();
+    log::info!("estimating used rows");
+    let row_usage = <prover::zkevm::circuit::SuperCircuit as TargetCircuit>::Inner::min_num_rows_block_subcircuits(&witness_block);
+    let r = row_usage
+        .iter()
+        .max_by_key(|x| x.row_num_real)
+        .unwrap()
+        .clone();
+    log::info!("final rows: {} {}", r.row_num_real, r.name);
 }
 
 #[cfg(feature = "prove_verify")]
