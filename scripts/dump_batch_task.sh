4fecfaf31c8bdd9d3efd6e7ad77cc1d61e103183
Zhang Zhuo <mycinbrin@gmail.com>
2024-05-30 10:01:41 +0800

    rename

--- scripts/dump_batch_task.sh
+++ scripts/dump_batch_task.sh
@@ -8,24 +8,17 @@ if [[ ! $batch_index =~ ^[0-9]+$ ]]; then
     exit
 fi
 
-# Set ENV DB_HOST, DB_USER and DB_NAME, and set password as:
-# https://www.postgresql.org/docs/current/libpq-pgpass.html
-db_host=$DB_HOST
-db_user=$DB_USER
-db_name=$DB_NAME
-if [ -z $db_host ] || [ -z $db_user ] || [ -z $db_name ]; then
-    echo "Must set ENV DB_HOST, DB_USER and DB_NAME!"
-    exit
-fi
+# psql -h $db_host -p $db_port -U $db_user $db_name
+db_conn=""
 
 # Replace with ENV OUTPUT_FILE.
-output_file="${OUTPUT_FILE:-"full_proof_1.json"}"
+output_file="${OUTPUT_FILE:-"batch_task.json"}"
 
 # Replace with ENV CHAIN_ID.
 chain_id="${CHAIN_ID:-534351}"
 
 # Get chunk-infos.
-chunk_infos=$(psql -h $db_host -U $db_user $db_name --csv -c "
+chunk_infos=$($db_conn --csv -c "
 select json_agg(res) as infos from (
     select
         $chain_id as chain_id,
@@ -43,7 +36,7 @@ chunk_infos=$(echo $chunk_infos | sed 's/""/"/g')
 chunk_infos=$(echo $chunk_infos | sed 's/^infos "\(.*\)"$/"chunk_infos": \1/g')
 
 # Get chunk-proofs.
-chunk_proofs=$(psql -h $db_host -U $db_user $db_name --csv -c "
+chunk_proofs=$($db_conn --csv -c "
 select convert_from(chunk.proof, 'UTF-8') as proofs
     from chunk join batch on chunk.batch_hash = batch.hash
     where batch.index = $batch_index
