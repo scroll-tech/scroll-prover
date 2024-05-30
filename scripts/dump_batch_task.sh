013530ff98c4d9b8a29ac404d4b480faedeb2091
Zhang Zhuo <mycinbrin@gmail.com>
2024-05-30 10:55:37 +0800

    chunk_info no longer needed

--- scripts/dump_batch_task.sh
+++ scripts/dump_batch_task.sh
@@ -17,24 +17,6 @@ output_file="${OUTPUT_FILE:-"batch_task.json"}"
 # Replace with ENV CHAIN_ID.
 chain_id="${CHAIN_ID:-534351}"
 
-# Get chunk-infos.
-chunk_infos=$($db_conn --csv -c "
-select json_agg(res) as infos from (
-    select
-        $chain_id as chain_id,
-        false as is_padding,
-        chunk.state_root              as post_state_root,
-        chunk.parent_chunk_state_root as prev_state_root,
-        chunk.withdraw_root           as withdraw_root,
-        chunk.hash                    as data_hash
-    from chunk join batch on chunk.batch_hash = batch.hash
-    where batch.index = $batch_index
-    order by chunk.index
-) res;")
-
-chunk_infos=$(echo $chunk_infos | sed 's/""/"/g')
-chunk_infos=$(echo $chunk_infos | sed 's/^infos "\(.*\)"$/"chunk_infos": \1/g')
-
 # Get chunk-proofs.
 chunk_proofs=$($db_conn --csv -c "
 select convert_from(chunk.proof, 'UTF-8') as proofs
@@ -46,4 +28,4 @@ chunk_proofs=$(echo $chunk_proofs | sed 's/" "/,/g')
 chunk_proofs=$(echo $chunk_proofs | sed 's/""/"/g')
 chunk_proofs=$(echo $chunk_proofs | sed 's/^proofs "\(.*\)"$/"chunk_proofs": [\1]/g')
 
-echo "{$chunk_infos,$chunk_proofs}" | jq > $output_file
+echo "{$chunk_proofs}" | jq > $output_file
