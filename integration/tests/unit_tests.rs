7323795f9575f94d3ca9c4e3f99548dc702063e8
Zhang Zhuo <mycinbrin@gmail.com>
2024-06-13 12:52:22 +0800

    Release v0.11.1 (#318)
    * commit
    
    * add assets
    
    * add evm verifier test

--- integration/tests/unit_tests.rs
+++ integration/tests/unit_tests.rs
@@ -5,6 +5,7 @@ use integration::test_util::{
     run_circuit_capacity_checker,
 };
 use prover::{
+    io::read_all,
     utils::{init_env_and_log, short_git_version},
     zkevm::circuit::{block_traces_to_witness_block, TargetCircuit},
 };
@@ -17,6 +18,51 @@ fn test_short_git_version() {
     log::info!("short_git_version = {git_version}");
 
     assert_eq!(git_version.len(), 7);
+}
+
+#[ignore]
+#[test]
+fn test_evm_verifier() {
+    init_env_and_log("test_evm_verifer");
+    log::info!("cwd {:?}", std::env::current_dir());
+    let version = "release-v0.11.1";
+    let yul = read_all(&format!("../{version}/evm_verifier.yul"));
+    //log::info!("yul len {}", yul.len());
+    let pi = read_all(&format!("../{version}/pi_data.data"));
+    let mut proof = read_all(&format!("../{version}/proof.data"));
+    proof.splice(384..384, pi);
+    log::info!("calldata len {}", proof.len());
+
+    for version in [
+        "0.8.19", "0.8.20", "0.8.21", "0.8.22", "0.8.23", "0.8.24", "0.8.25", "0.8.26",
+    ] {
+        use snark_verifier::loader::evm::compile_yul;
+        use std::process::Command;
+        Command::new("svm")
+            .arg("use")
+            .arg(version)
+            .output()
+            .expect("failed to execute process");
+        log::info!("svm use {}", version);
+        let bytecode = compile_yul(&String::from_utf8(yul.clone()).unwrap());
+        log::info!("bytecode len {}", bytecode.len());
+        match integration::evm::deploy_and_call(bytecode, proof.clone()) {
+            Ok(gas) => log::info!("gas cost {gas}"),
+            Err(e) => {
+                panic!("test failed {e:#?}");
+            }
+        }
+    }
+
+    log::info!("check released bin");
+    let bytecode = read_all(&format!("../{version}/evm_verifier.bin"));
+    log::info!("bytecode len {}", bytecode.len());
+    match integration::evm::deploy_and_call(bytecode, proof.clone()) {
+        Ok(gas) => log::info!("gas cost {gas}"),
+        Err(e) => {
+            panic!("test failed {e:#?}");
+        }
+    }
 }
 
 #[test]
