dbd45a7e2140403b557a3eeba5fdcd577fd19de0
Zhuo Zhang <mycinbrin@gmail.com>
2024-06-24 14:47:31 +0800

    0.11.4

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
-IN=integration/outputs/e2e_tests_20240617_143651/
-OUT=release-v0.11.3
+IN=integration/outputs/e2e_tests_20240622_061337
+OUT=release-v0.11.4
 
 mkdir -p $OUT
 cp $IN/chunk_chunk_0.protocol $OUT/chunk.protocol
