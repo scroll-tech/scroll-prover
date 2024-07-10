af1437cf4d29492db71d1ee02c5eaeb8059f8be8
Zhuo Zhang <mycinbrin@gmail.com>
2024-07-10 20:49:01 +0800

    assets for release-v0.12.0-rc.1

--- release.sh
+++ release.sh
@@ -1,22 +1,21 @@
-IN=integration/outputs/e2e_tests_20240622_061337
-OUT=release-v0.12.0
+IN=integration/outputs/e2e_tests
+OUT=release-v0.12.0-rc.1
 
 mkdir -p $OUT
 
 # copy snark protocol and verification keys
 cp $IN/chunk_chunk_0.protocol $OUT/chunk.protocol
-cp $IN/batch.protocol $OUT/batch.protocol
-cp $IN/vk_chunk.vkey $OUT/vk_chunk.vkey
-cp $IN/vk_batch.vkey $OUT/vk_batch.vkey
-cp $IN/vk_bundle.vkey $OUT/vk_bundle.key
+cp $IN/vk_chunk_0.vkey $OUT/vk_chunk.vkey
+cp $IN/vk_batch_agg.vkey $OUT/vk_batch.vkey
+cp $IN/vk_bundle_recursion.vkey $OUT/vk_bundle.key
 
 # copy verifier contract binary
 cp $IN/evm_verifier.bin $OUT/evm_verifier.bin
 cp $IN/evm_verifier.yul $OUT/evm_verifier.yul
 
 # copy public input and proof for the outermost circuit (recursive bundler)
-cp $IN/pi_bundle.data $OUT/pi.data
-cp $IN/proof_bundle.data $OUT/proof.data
+cp $IN/pi_bundle_recursion.data $OUT/pi.data
+cp $IN/proof_bundle_recursion.data $OUT/proof.data
 
 # copy config values for each proving layer
 cp ./integration/configs/* $OUT
