810ed7315dcfc072e3e507108b047102cf0e8f2a
Rohit Narurkar <rohit.narurkar@proton.me>
2024-07-12 11:35:01 +0100

    redirect proprocessed data to separate hex file in release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 IN=integration/outputs/e2e_tests
-OUT=release-v0.12.0-rc.1
+OUT=release-v0.12.0-rc.2
 
 mkdir -p $OUT
 
@@ -16,6 +16,10 @@ cp $IN/evm_verifier.yul $OUT/evm_verifier.yul
 # copy public input and proof for the outermost circuit (recursive bundler)
 cp $IN/pi_bundle_recursion.data $OUT/pi.data
 cp $IN/proof_bundle_recursion.data $OUT/proof.data
+
+# dump the preprocessed digest into a separate hex file.
+# preprocessed digest is the first 32 bytes of the public input.
+xxd -l 32 -p $OUT/pi.data | tr -d '\n' | awk '{gsub("%", ""); print}' > $OUT/preprocessed_digest.hex
 
 # copy config values for each proving layer
 cp ./integration/configs/* $OUT
