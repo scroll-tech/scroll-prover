39ab11777f87a8c3c782ee9a20cd8ed720a7f2ae
Zhuo Zhang <mycinbrin@gmail.com>
2024-08-27 13:39:50 +0000

    fmt

--- integration/src/l2geth.rs
+++ integration/src/l2geth.rs
@@ -22,7 +22,8 @@ impl Client {
         Ok(self.provider.get_block_number().await?.as_u64())
     }
     pub async fn get_txbytx_trace_by_num(&self, block_num: i64) -> Result<Vec<BlockTrace>> {
-        let params = serde_json::json!([format!("{block_num:#x}"), {"StorageProofFormat": "legacy"}]);
+        let params =
+            serde_json::json!([format!("{block_num:#x}"), {"StorageProofFormat": "legacy"}]);
         let trace = self
             .provider
             .request("scroll_getTxByTxBlockTrace", params)
