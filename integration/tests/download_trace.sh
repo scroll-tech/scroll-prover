dd07ca93ad446614c4017f0f0451f92173d92d10
Zhuo Zhang <mycinbrin@gmail.com>
2024-09-05 06:38:53 +0000

    update trace

---
+++ integration/tests/download_trace.sh
@@ -0,0 +1,4 @@
+set -x
+blk=$1
+curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"scroll_getBlockTraceByNumberOrHash", "params": ["'$(printf '0x%x' $blk)'"], "id": 99}' 127.0.0.1:8545 | jq .result >/tmp/${blk}.json
+#curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"scroll_getTxByTxBlockTrace", "params": ["'$(printf '0x%x' $blk)'", null], "id": 99}' 127.0.0.1:8545 | jq .result >/tmp/${blk}.json
