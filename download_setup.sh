1a5037677766761e12dd49b0565f5c1bf26938e6
Matthias Goergens <matthias.goergens@gmail.com>
2024-10-01 14:36:58 +0800

    Use long options

--- download_setup.sh
+++ download_setup.sh
@@ -6,10 +6,10 @@ degree="${SCROLL_PROVER_MAX_DEGREE:-${1:-26}}"
 
 # Set the output dir to second input or default as `./integration/params`.
 params_dir="${2:-"./integration/params"}"
-mkdir -p "$params_dir"
+mkdir --parents "$params_dir"
 
 output_file="$params_dir"/params"${degree}"
-rm -f "$output_file"
+rm --force "$output_file"
 
 # degree 1 - 26
-axel -ac https://circuit-release.s3.us-west-2.amazonaws.com/setup/params"$degree" -o "$output_file"
+axel --alternate --no-clobber https://circuit-release.s3.us-west-2.amazonaws.com/setup/params"$degree" --output="$output_file"
