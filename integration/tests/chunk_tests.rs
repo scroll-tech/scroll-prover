6a6c4678c2da965186cfd6a7c02b54a6ff486d2a
noelwei <fan@scroll.io>
2024-10-06 23:53:48 +0900

    resume path extracting
    Signed-off-by: noelwei <fan@scroll.io>

--- integration/tests/chunk_tests.rs
+++ integration/tests/chunk_tests.rs
@@ -31,6 +31,7 @@ fn test_chunk_prove_verify() {
 #[test]
 fn test_sp1_chunk_prove_verify() {
     use itertools::Itertools;
+    use std::path::Path;
     let output_dir = init_env_and_log("chunk_tests");
     log::info!("Initialized ENV and created output-dir {output_dir}");
 
@@ -40,10 +41,23 @@ fn test_sp1_chunk_prove_verify() {
     );
 
     let trace_path = trace_path_for_test();
+
+    // suppose the snark is put in the same path with trace_path
+    // and we extract the path part
+    let trace_asset_path = Path::new(&trace_path);
+    let trace_asset_path = if trace_asset_path.is_dir() {
+        trace_asset_path.to_path_buf()
+    } else {
+        trace_asset_path.parent()
+        .map(|p| p.to_path_buf())
+        .unwrap_or_else(|| Path::new(".").to_path_buf())
+    };
+
     let traces = load_chunk(&trace_path).1;
     let chunk = ChunkProvingTask::from(traces);
     let mut prover = SP1Prover::from_params_and_assets(&params_map, ASSETS_DIR);
     log::info!("Constructed sp1 chunk prover");
-    prove_and_verify_sp1_chunk(&params_map, &output_dir, Some(&trace_path), chunk, &mut prover, None);
+    prove_and_verify_sp1_chunk(&params_map, &output_dir, 
+        Some(trace_asset_path.to_str().unwrap()), chunk, &mut prover, None);
 
 }
