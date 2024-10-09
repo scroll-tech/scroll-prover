99f664f306c8ddec70b7a56078316a4914b620d0
noelwei <fan@scroll.io>
2024-10-09 16:26:08 +0900

    add test for evm verifier
    Signed-off-by: noelwei <fan@scroll.io>

--- integration/tests/bundle_tests.rs
+++ integration/tests/bundle_tests.rs
@@ -37,3 +37,41 @@ fn gen_bundle_proving_task(batch_proof_files: &[&str]) -> BundleProvingTask {
 
     BundleProvingTask { batch_proofs }
 }
+
+#[test]
+fn test_evm_verifier_from_layer5() {
+    use prover::{
+        io::load_snark,
+        config::LayerId,
+        utils::read_env_var, 
+    };
+    use integration::test_util::PARAMS_DIR;
+    use itertools::Itertools;
+    use prover::config::AGG_DEGREES;
+
+    let output_dir = init_env_and_log("test_evm_verifer");
+
+    let layer5_snark_path = read_env_var(
+        "LAYER5_SNARK", 
+        "tests/test_data/recursion_snark_layer5.json".to_string());
+    let snark = load_snark(&layer5_snark_path).ok().flatten().unwrap();
+
+    let params_map = prover::common::Prover::load_params_map(
+        PARAMS_DIR,
+        &AGG_DEGREES.iter().copied().collect_vec(),
+    );
+
+    let mut evm_prover = prover::common::Prover::from_params_map(&params_map);
+
+    // enforce general yul
+    std::env::set_var("SCROLL_PROVER_DUMP_YUL", "true");
+    evm_prover.load_or_gen_comp_evm_proof(
+        "0",
+        LayerId::Layer6.id(),
+        true,
+        LayerId::Layer6.degree(),
+        snark,
+        Some(output_dir.as_str()),
+    ).unwrap();
+
+}
