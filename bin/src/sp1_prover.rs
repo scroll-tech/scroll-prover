e3c63694993b40d51e990c72ef52bc8facc6e4ea
noelwei <fan@scroll.io>
2024-11-15 09:39:34 +0800

    add default sp1-halo2-backend param
    Signed-off-by: noelwei <fan@scroll.io>

--- bin/src/sp1_prover.rs
+++ bin/src/sp1_prover.rs
@@ -22,6 +22,20 @@ struct Args {
     trace_path: String,
 }
 
+const DEFAULT_PARAM : &str = 
+r#"{
+  "strategy": "Vertical",
+  "k": 24,
+  "num_advice_per_phase": [
+    3
+  ],
+  "num_fixed": 1,
+  "num_lookup_advice_per_phase": [
+    1
+  ],
+  "lookup_bits": 22
+}"#;
+
 fn main() {
     // Layer config files are located in `./integration/configs`.
     env::set_current_dir("./integration").unwrap();
@@ -42,6 +56,7 @@ fn main() {
         ],
     );
 
+    env::set_var("BASE_CONFIG_PARAMS", DEFAULT_PARAM);
     let mut prover = Sp1Prover::from_params_and_assets(&params_map, &args.assets_path);
     log::info!("Constructed sp1 prover");
 
