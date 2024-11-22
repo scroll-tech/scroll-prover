4a95a63dd64b5b987b27719276ba6617a3aa0353
Zhuo Zhang <mycinbrin@gmail.com>
2024-11-22 05:24:52 +0000

    add circuit-assets.txt

--- release.sh
+++ release.sh
@@ -1,29 +1,44 @@
-IN=integration/outputs/e2e_tests_20240819_131744/
-OUT=release-v0.13.0
+CIRCUIT_VERSION="v0.13.1"
 
-mkdir -p $OUT
+function download_s3() {
+  OUT=release-${CIRCUIT_VERSION}
+  aws --profile default s3 cp s3://circuit-release/$OUT $OUT --recursive
+}
 
-# copy snark protocol and verification keys
-cp $IN/chunk_chunk_0.protocol $OUT/chunk.protocol
-cp $IN/vk_chunk_0.vkey $OUT/vk_chunk.vkey
-cp $IN/vk_batch_agg.vkey $OUT/vk_batch.vkey
-cp $IN/vk_bundle_recursion.vkey $OUT/vk_bundle.vkey
+function upload_s3() {
+  IN=integration/outputs/e2e_tests_20240819_131744/
+  OUT=release-${CIRCUIT_VERSION}
 
-# copy verifier contract binary
-cp $IN/evm_verifier.bin $OUT/evm_verifier.bin
-cp $IN/evm_verifier.yul $OUT/evm_verifier.yul
+  mkdir -p $OUT
 
-# copy public input and proof for the outermost circuit (recursive bundler)
-cp $IN/pi_bundle_recursion.data $OUT/pi.data
-cp $IN/proof_bundle_recursion.data $OUT/proof.data
+  # copy snark protocol and verification keys
+  cp $IN/chunk_chunk_0.protocol $OUT/chunk.protocol
+  cp $IN/vk_chunk_0.vkey $OUT/vk_chunk.vkey
+  cp $IN/vk_batch_agg.vkey $OUT/vk_batch.vkey
+  cp $IN/vk_bundle_recursion.vkey $OUT/vk_bundle.vkey
 
-# dump the preprocessed digest into a separate hex file.
-# preprocessed digest is the first 32 bytes of the public input.
-xxd -l 32 -p $OUT/pi.data | tr -d '\n' | awk '{gsub("%", ""); print}' > $OUT/preprocessed_digest.hex
+  # copy verifier contract binary
+  cp $IN/evm_verifier.bin $OUT/evm_verifier.bin
+  cp $IN/evm_verifier.yul $OUT/evm_verifier.yul
 
-# copy config values for each proving layer
-cp ./integration/configs/* $OUT
+  # copy public input and proof for the outermost circuit (recursive bundler)
+  cp $IN/pi_bundle_recursion.data $OUT/pi.data
+  cp $IN/proof_bundle_recursion.data $OUT/proof.data
 
-cd $OUT; sha256sum * > sha256sum; cd ..
+  # dump the preprocessed digest into a separate hex file.
+  # preprocessed digest is the first 32 bytes of the public input.
+  xxd -l 32 -p $OUT/pi.data | tr -d '\n' | awk '{gsub("%", ""); print}' >$OUT/preprocessed_digest.hex
 
-aws --profile default s3 cp $OUT s3://circuit-release/$OUT --recursive
+  # copy config values for each proving layer
+  cp ./integration/configs/* $OUT
+
+  cd $OUT
+  sha256sum * > sha256sum
+  cd ..
+  cd integration/params/; sha256sum * >> ../../$OUT/sha256sum; cd ../..
+
+  aws --profile default s3 cp $OUT s3://circuit-release/$OUT --recursive
+}
+
+#upload_s3
+download_s3
